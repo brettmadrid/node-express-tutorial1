dfde596eb276b69282e2ed0f499377c62f634aed
brettmadrid <brettmadrid9@gmail.com>
2020-03-17 15:03:48 -0700

    added post endpoints

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const server = express();
 
+server.use(express.json());
+
 const PORT = 5000;
+
+const channels = [];
+const lessons = [];
 
 server.get("/", (req, res) => {
   res.json({ hello: "world" });
@@ -10,6 +16,20 @@ server.get("/", (req, res) => {
 
 server.get("/hello", (req, res) => {
   res.json({ hello: "future senior software developer" });
+});
+
+server.post("/api/channels", (req, res) => {
+  const channelInfo = req.body;
+  channelInfo.id = shortid.generate();
+  channels.push(channelInfo);
+  res.status(201).json(channelInfo);
+});
+
+server.post("/api/lessons", (req, res) => {
+  const lessonInfo = req.body;
+  lessonInfo.id = shortid.generate();
+  lessons.push(lessonInfo);
+  res.status(201).json(lessonInfo);
 });
 
 server.listen(5000, () => {
